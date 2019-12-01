1f20724b84798d146a08ca1022d1dc050f17da8a
JRoy <joshroy126@gmail.com>
2019-12-01 09:48:13 -0500

    Follow code conventions on Main

--- src/main/java/com/apickledwalrus/papiaddon/Main.java
+++ src/main/java/com/apickledwalrus/papiaddon/Main.java
@@ -1,27 +1,27 @@
 package com.apickledwalrus.papiaddon;
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.SkriptAddon;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import java.io.IOException;
 
 public class Main extends JavaPlugin {
-	private static Main instance;
+  private static Main instance;
+
+  public static Main getInstance() {
+    if (instance == null) {
+      throw new IllegalStateException();
+    }
+    return instance;
+  }
 
   @Override
-	public void onEnable() {
-		instance = this;
-		try {
-			Skript.registerAddon(this).loadClasses("com.apickledwalrus.papiaddon.skript", "events", "expressions");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static Main getInstance() {
-		if (instance == null) {
-			throw new IllegalStateException();
-		}
-		return instance;
-	}
+  public void onEnable() {
+    instance = this;
+    try {
+      Skript.registerAddon(this).loadClasses("com.apickledwalrus.papiaddon.skript", "events", "expressions");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
