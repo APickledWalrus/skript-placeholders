e975d198f0a5ee4b1e16353aa21e36024b5f77d1
APickledWalrus <apickledwalrus@gmail.com>
2024-08-09 22:06:32 -0400

    Add Skript version checks

--- src/main/java/io/github/apickledwalrus/skriptplaceholders/SkriptPlaceholders.java
+++ src/main/java/io/github/apickledwalrus/skriptplaceholders/SkriptPlaceholders.java
@@ -1,8 +1,10 @@
 package io.github.apickledwalrus.skriptplaceholders;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.util.Version;
 import io.github.apickledwalrus.skriptplaceholders.placeholder.PlaceholderPlugin;
 import io.github.apickledwalrus.skriptplaceholders.placeholder.PlaceholderRegistry;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -21,12 +23,26 @@ public class SkriptPlaceholders extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		instance = this;
+		Plugin skript = getServer().getPluginManager().getPlugin("Skript");
+		if (skript == null || !skript.isEnabled()) {
+			getLogger().severe("Could not find Skript. Make sure that you have it installed and that it is properly loaded. Disabling...");
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
+		if (Skript.getVersion().isSmallerThan(new Version(2, 7, 3))) {
+			getLogger().severe("You are running an unsupported version of Skript. Please update to at least Skript 2.7.3. Disabling...");
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
+
 		if (PlaceholderPlugin.getInstalledPlugins().isEmpty()) {
 			getLogger().severe("No placeholder plugins were found. Do you have any installed? Disabling...");
 			getServer().getPluginManager().disablePlugin(this);
 			return;
 		}
+
+		instance = this;
+		registry = new PlaceholderRegistry(this);
 
 		try {
 			Skript.registerAddon(this).loadClasses("io.github.apickledwalrus.skriptplaceholders.skript.elements");
@@ -35,13 +51,12 @@ public class SkriptPlaceholders extends JavaPlugin {
 			getLogger().severe(e.toString());
 			getServer().getPluginManager().disablePlugin(this);
 		}
-
-		this.registry = new PlaceholderRegistry(this);
 	}
 
 	@Override
 	public void onDisable() {
 		instance = null;
+		registry = null;
 	}
 
 	public PlaceholderRegistry getRegistry() {
